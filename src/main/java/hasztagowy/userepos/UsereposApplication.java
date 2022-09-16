d397aa706ed558232fb4925373c3ed77df63f0f3
hasztagowy <miszorkappa123@gmail.com>
2022-09-16 18:21:34 +0200

    init

---
+++ src/main/java/hasztagowy/userepos/UsereposApplication.java
@@ -0,0 +1,13 @@
+package hasztagowy.userepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsereposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsereposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hasztagowy/userepos/UsereposApplicationTests.java
@@ -0,0 +1,13 @@
+package hasztagowy.userepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsereposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
