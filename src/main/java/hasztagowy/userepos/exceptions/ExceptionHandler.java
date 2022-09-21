54229f10251a15d02f18e07ebc78c8f46591906a
hasztagowy <miszorkappa123@gmail.com>
2022-09-21 21:30:31 +0200

    add exception

--- src/main/java/hasztagowy/userepos/exceptions/ExceptionHandler.java
+++ src/main/java/hasztagowy/userepos/exceptions/ExceptionHandler.java
@@ -9,11 +9,21 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(value = UserNotFoundException.class)
-    public ResponseEntity<Object> userNotFoundEx(UserNotFoundException e){
+    public ResponseEntity<Object> userNotFoundEx(UserNotFoundException e) {
 
-        HttpStatus httpStatus=HttpStatus.BAD_REQUEST;
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
-        ExceptionModel exceptionModel= new ExceptionModel(httpStatus.value(),e.getMessage());
+        ExceptionModel exceptionModel = new ExceptionModel(httpStatus.value(), e.getMessage());
+
+        return new ResponseEntity<>(exceptionModel, httpStatus);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = UserExistException.class)
+    public ResponseEntity<Object> userExist(UserNotFoundException e) {
+
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        ExceptionModel exceptionModel = new ExceptionModel(httpStatus.value(), e.getMessage());
 
         return new ResponseEntity<>(exceptionModel, httpStatus);
     }
