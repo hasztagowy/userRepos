a301a300bd94c10cde4a2e36cc1a02778de03975
Robert Bączkiewicz <miszorkappa123@gmail.com>
2022-09-22 08:53:09 +0200

    change mapping, add delete user

--- src/main/java/hasztagowy/userepos/controller/AuthUserController.java
+++ src/main/java/hasztagowy/userepos/controller/AuthUserController.java
@@ -2,6 +2,7 @@ package hasztagowy.userepos.controller;
 
 import hasztagowy.userepos.exceptions.ExceptionModel;
 import hasztagowy.userepos.exceptions.UserExistException;
+import hasztagowy.userepos.exceptions.UserNotFoundException;
 import hasztagowy.userepos.model.AuthUserModel;
 import hasztagowy.userepos.service.AuthUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/v1")
+@RestController
+@RequestMapping("/v1")
 public class AuthUserController {
 
     AuthUserService authUserService;
@@ -19,7 +21,7 @@ public class AuthUserController {
         this.authUserService = authUserService;
     }
 
-    @PostMapping("/user")
+    @PostMapping("/users")
     public ResponseEntity<?> addNewUser(@RequestBody AuthUserModel authUserModel) {
         try {
             return authUserService.saveUser(authUserModel);
@@ -28,8 +30,17 @@ public class AuthUserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
         }
     }
-    @PutMapping("user/changePassword")
+    @PutMapping("/users/changePassword")
     public ResponseEntity<?> changePassword(@RequestBody AuthUserModel authUserModel){
         return null;
+    }
+
+    @DeleteMapping("/users")
+
+    public ResponseEntity<?> deleteUser(@RequestParam String name) throws UserNotFoundException {
+        if(name.isEmpty() || name.isBlank()){
+            throw  new UserNotFoundException("user not found");
+        }
+      return authUserService.deleteUser(name);
     }
 }
--- src/main/java/hasztagowy/userepos/controller/RepositoriesController.java
+++ src/main/java/hasztagowy/userepos/controller/RepositoriesController.java
@@ -6,6 +6,7 @@ import hasztagowy.userepos.service.RepositoriesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -14,14 +15,11 @@ import java.util.Optional;
 
 @RestController
 @Validated
+@RequestMapping("/v1")
 public class RepositoriesController {
 
     @Autowired
     private RepositoriesServiceImpl repositoriesServiceImpl;
-
-    public RepositoriesController(RepositoriesServiceImpl repositoriesServiceImpl) {
-        this.repositoriesServiceImpl = repositoriesServiceImpl;
-    }
 
     @GetMapping("/getUserRepositories")
     public Flux<RepositoryModel> getUserRepository(@RequestParam String user,
