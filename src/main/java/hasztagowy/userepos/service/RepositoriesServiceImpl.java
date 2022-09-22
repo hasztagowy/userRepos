4af5a646e91a63a225c69c7348b6279b4ebaa5cb
Robert Bączkiewicz <miszorkappa123@gmail.com>
2022-09-22 07:41:56 +0200

    refactor

--- src/main/java/hasztagowy/userepos/service/RepositoriesServiceImpl.java
+++ src/main/java/hasztagowy/userepos/service/RepositoriesServiceImpl.java
@@ -27,23 +27,23 @@ public class RepositoriesServiceImpl {
         this.webClientBuilder = webClientBuilder;
     }
 
-    public Flux<RepositoryModel> getUserRepositories(String name, Optional<String> sort, Optional<String>  orderby, Optional<String> type){
+    public Flux<RepositoryModel> getUserRepositories(String name, Optional<String> sort, Optional<String> orderby, Optional<String> type) {
 
-        uri="https://api.github.com/users/"+name+"/repos";
+        uri = "https://api.github.com/users/" + name + "/repos";
 
-        String url= ValidateParams.generateUrl(uri,sort,orderby);
+        String url = ValidateParams.generateUrl(uri, sort, orderby);
 
-        page=1;
+        page = 1;
 
-        Flux<RepositoryModel> repositoryFlux=getData(url);
+        Flux<RepositoryModel> repositoryFlux = getData(url);
 
 
-        if(type.isPresent()) {
-            return repositoryFlux.map(x->{
+        if (type.isPresent()) {
+            return repositoryFlux.map(x -> {
                 x.setUpdatedRecently(RepositoryModel.checkLastUpdateDate(x.getUpdatedAt()));
                 return x;
-            }).filter(repositoryModel -> repositoryModel.getUpdatedRecently()==Boolean.valueOf(type.get()));
-        }else {
+            }).filter(repositoryModel -> repositoryModel.getUpdatedRecently() == Boolean.valueOf(type.get()));
+        } else {
             return repositoryFlux.map(x -> {
                 x.setUpdatedRecently(RepositoryModel.checkLastUpdateDate(x.getUpdatedAt()));
                 return x;
@@ -51,28 +51,29 @@ public class RepositoriesServiceImpl {
         }
     }
 
-    private Flux<RepositoryModel> getData(String url){
-        return getNextPage(url,page)
+    private Flux<RepositoryModel> getData(String url) {
+        return getNextPage(url, page)
                 .expand(fluxResponseEntity -> {
                     fluxResponseEntity.getHeaders().get("Link");
-                    if(fluxResponseEntity.getHeaders().get("Link")==null ||!(fluxResponseEntity.getHeaders().get("Link").toString().contains(("rel="+"\"next\"")))){
+                    if (fluxResponseEntity.getHeaders().get("Link") == null || !(fluxResponseEntity.getHeaders().get("Link").toString().contains(("rel=" + "\"next\"")))) {
                         return Mono.empty();
                     }
                     page++;
                     System.out.println(page);
-                    return getNextPage(url,page);
+                    return getNextPage(url, page);
                 })
                 .flatMap(HttpEntity::getBody);
     }
-    private Mono<ResponseEntity<Flux<RepositoryModel>>> getNextPage(String url, int page){
+
+    private Mono<ResponseEntity<Flux<RepositoryModel>>> getNextPage(String url, int page) {
 
         return webClientBuilder.build()
                 .get()
-                .uri(url+"&page="+page)
+                .uri(url + "&page=" + page)
                 .retrieve()
-                .onStatus(httpStatus -> httpStatus.value()== HttpStatus.INTERNAL_SERVER_ERROR.value(), clientResponse -> Mono.error(new ServiceUnavailableException("Service is not available")))
-                .onStatus(httpStatus -> httpStatus.value()==HttpStatus.FORBIDDEN.value(),clientResponse -> Mono.error(new ServiceUnavailableException("Forbiden")))
-                .onStatus(HttpStatus::is4xxClientError, clientResponse ->Mono.error(new UserNotFoundException("User not found")))
+                .onStatus(httpStatus -> httpStatus.value() == HttpStatus.INTERNAL_SERVER_ERROR.value(), clientResponse -> Mono.error(new ServiceUnavailableException("Service is not available")))
+                .onStatus(httpStatus -> httpStatus.value() == HttpStatus.FORBIDDEN.value(), clientResponse -> Mono.error(new ServiceUnavailableException("Forbiden")))
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new UserNotFoundException("User not found")))
                 .toEntityFlux(RepositoryModel.class);
     }
 }
